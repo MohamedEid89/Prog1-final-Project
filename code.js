43c7608866381aa65eb4b38a5499a26b508d08c6
Mohammed Eid <info@welayer.se>
2021-03-22 23:59:29 +0100

    Last

--- code.js
+++ code.js
@@ -22,11 +22,11 @@ function getMealList(){
                 html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "Mat">
+                            <img src = "${meal.strMealThumb}" alt = "Bild">
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">hitta Recepet</a>
+                            <a href = "recipe-close-btn" class = "recept-btn">Hitta recept</a>
                         </div>
                     </div>
                 `;
@@ -41,7 +41,6 @@ function getMealList(){
     });
 }
 
-
 // get recept of the meal
 function getMealRecipe(e){
     e.preventDefault();
@@ -53,24 +52,3 @@ function getMealRecipe(e){
     }
 }
 
-// create a modal
-function mealRecipeModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
-}
